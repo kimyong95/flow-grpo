9a7c45c6a3cb0eb6ac8abf7a54066e896ebeebf3
Tan Kim Yong <kimyong95@gmail.com>
2025-08-28 22:49:25 +0800

    update

--- config/optimize.py
+++ config/optimize.py
@@ -4,33 +4,6 @@ import os
 
 base = imp.load_source("base", os.path.join(os.path.dirname(__file__), "base.py"))
 
-def ocr():
-    config = base.get_config()
-    config.max_epochs = 500
-    config.eval_freq = 10
-
-    config.run_name = "ocr"
-    config.compile = True
-
-    config.pretrained.model = "stabilityai/stable-diffusion-3.5-medium"
-    config.sample.num_steps = 40
-    config.sample.eval_num_steps = 40
-    config.sample.guidance_scale = 4.5
-
-    config.sample.num_batches_per_epoch = 1
-    config.sample.train_batch_size = 32
-    config.sample.test_batch_size = 32
-
-    config.dataset = os.path.join(os.getcwd(), "dataset/ocr_1")
-
-    config.prompt_fn = "general_ocr"
-
-    config.reward_fn = {"ocr": 1.0,}
-
-    return config
-
-
-
 def compressibility():
     config = base.get_config()
     config.max_epochs = 500
@@ -43,6 +16,7 @@ def compressibility():
     config.sample.num_steps = 40
     config.sample.eval_num_steps = 40
     config.sample.guidance_scale = 4.5
+    config.resolution = 512
 
     config.sample.num_batches_per_epoch = 1
     config.sample.train_batch_size = 32
@@ -69,6 +43,7 @@ def prompt_align():
     config.sample.num_steps = 40
     config.sample.eval_num_steps = 40
     config.sample.guidance_scale = 4.5
+    config.resolution = 512
 
     # batching (keep consistent with other optimize presets)
     config.sample.num_batches_per_epoch = 1
